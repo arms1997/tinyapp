f0b250daf33cf52dbd6fc8f1ebb7193d3d5a49d5
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-01-19 13:10:56 -0500

    updated server.js, and created randomStringGenerator

--- server.js
+++ server.js
@@ -14,33 +14,49 @@ const urlDatabase = {
 };
 
 app.get('/', (req, res) => {
-  res.redirect('/urls')
+  res.redirect('/urls');
 });
 
 app.get('/urls/new', (req, res) => {
-  res.render('urls_new')
-})
+  res.render('urls_new');
+});
 
 app.get('/urls.json', (req, res) => {
-  res.json(urlDatabase)
-})
+  res.json(urlDatabase);
+});
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase }
-  console.log(templateVars)
-  res.render('urls_index', templateVars)
-})
+  const templateVars = { urls: urlDatabase };
+  console.log(templateVars);
+  res.render('urls_index', templateVars);
+});
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
+  if(!urlDatabase[req.params.shortURL]){
+    res.end('shortURL does not exist');
+  }
 
-  res.render('urls_show', templateVars)
+  const templateVars = {
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL]
+  };
+
+  res.render('urls_show', templateVars);
+});
+
+app.get('/u/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+
+  res.redirect(longURL);
 })
 
-app.post('/url', (req, res) => {
-  console.log(req.body)
-  res.send('ok')
-})
+app.post('/urls', (req, res) => {
+  let randomString = generateRandomString();
+
+  urlDatabase[randomString] = req.body.longURL;
+
+  res.redirect(`/urls/${randomString}`);
+});
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
