ddff40053df7510176372157f21ed51ea2b4e1fb
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-01-20 14:38:58 -0500

    added a helper function to return the key of the requested user

--- helper_functions/checkIfEmailExists.js
+++ helper_functions/checkIfEmailExists.js
@@ -1,8 +1,22 @@
-module.exports.checkIfUserExists = (users, email) => {
+const checkIfUserExists = (users, email) => {
   for(let key in users){
     if(users[key].email === email){
       return true
     }
   }
   return false;
+}
+
+const findUser = (users, email, password) => {
+  for(let key in users){
+    if(users[key].email === email && users[key].password === password){
+      return key
+    }
+    return;
+  }
+}
+
+module.exports = {
+  checkIfUserExists,
+  findUser
 }
